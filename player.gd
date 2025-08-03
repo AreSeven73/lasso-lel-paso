6c3a5ec544fa94886526117307a8d2ea5eed16b1
AreSeven <zay759.xyz@gmail.com>
2025-08-03 18:30:33 +0900

    added player attack animation

--- player.gd
+++ player.gd
@@ -3,6 +3,9 @@ extends CharacterBody3D
 @export var speed = 3
 @export var jumpspeed = 6
 @export var fall_acceleration = 25
+var attacking = false
+var cardinal = "south"
+
 
 var target_velocity = Vector3.ZERO
 var direction = Vector3.ZERO
@@ -14,6 +17,71 @@ func _physics_process(delta):
 	vel = Vector2(velocity.x, velocity.z)
 	pos = Vector2(position.x, position.z)
 	direction = Vector3.ZERO
+	
+	
+	if not attacking:
+		if Input.is_action_pressed("action"):
+			attacking = true
+			#velocity.x = 0
+			#velocity.z = 0
+			$Timer.start(1.6)
+			$attack.visible = true
+			#$attack.rotation = direction
+			$attack/CollisionShape3D.disabled = false
+			$attack/AnimatedSprite3D.visible = false
+			$attack/AnimatedSprite3D2.visible = false
+			#$Pivot/AnimatedSprite3D.pause()
+			#$Pivot/AnimatedSprite3D.set_frame_and_progress(0,0.0)
+			$attack/AnimatedSprite3D.pause()
+			$attack/AnimatedSprite3D.set_frame_and_progress(0,0.0)
+			$attack/AnimatedSprite3D2.pause()
+			$attack/AnimatedSprite3D2.set_frame_and_progress(0,0.0)
+			if cardinal == "south":
+				$attack.rotation.y = PI
+				$attack/AnimatedSprite3D2.rotation.y = 0
+				$attack/AnimatedSprite3D2.visible = true
+				$attack/AnimatedSprite3D2.play("lasso north")
+			elif cardinal == "southeast":
+				$attack.rotation.y = -3*PI/4
+				$attack/AnimatedSprite3D2.rotation.y = 0
+				$attack/AnimatedSprite3D2.visible = true
+				$attack/AnimatedSprite3D2.play("lasso north")
+			elif cardinal == "east":
+				$attack.rotation.y = -PI/2
+				$attack/AnimatedSprite3D.visible = true
+				$attack/AnimatedSprite3D.play("lasso east")
+			elif cardinal == "northeast":
+				$attack.rotation.y = -PI/4
+				$attack/AnimatedSprite3D2.rotation.y = 0
+				$attack/AnimatedSprite3D2.visible = true
+				$attack/AnimatedSprite3D2.play("lasso north")
+			elif cardinal == "north":
+				$attack.rotation.y = 0
+				$attack/AnimatedSprite3D2.rotation.y = 0
+				$attack/AnimatedSprite3D2.visible = true
+				$attack/AnimatedSprite3D2.play("lasso north")
+			elif cardinal == "northwest":
+				$attack.rotation.y = PI/4
+				$attack/AnimatedSprite3D2.rotation.y = 0
+				$attack/AnimatedSprite3D2.visible = true
+				$attack/AnimatedSprite3D2.play("lasso north")
+			elif cardinal == "west":
+				$attack.rotation.y = PI/2
+				$attack/AnimatedSprite3D2.rotation.y = PI/2
+				$attack/AnimatedSprite3D.visible = true
+				$attack/AnimatedSprite3D.play("lasso east")
+			elif cardinal == "southwest":
+				$attack.rotation.y = 3*PI/4
+				$attack/AnimatedSprite3D2.rotation.y = 0	
+				$attack/AnimatedSprite3D2.visible = true
+				$attack/AnimatedSprite3D2.play("lasso north")
+			print(cardinal)
+			print($attack.rotation.y)
+			
+			
+	
+	
+
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -30,28 +98,37 @@ func _physics_process(delta):
 	elif direction.x == 0 and direction.z == 1:
 		$Pivot/AnimatedSprite3D.rotation.y = 0
 		$Pivot/AnimatedSprite3D.play("south")
+		cardinal = "south"
 	elif direction.x == 1 and direction.z == 1:
 		$Pivot/AnimatedSprite3D.rotation.y = 0
 		$Pivot/AnimatedSprite3D.play("southeast")
+		cardinal = "southeast"
 	elif direction.x == 1 and direction.z == 0:
 		$Pivot/AnimatedSprite3D.rotation.y = 0
 		$Pivot/AnimatedSprite3D.play("east")
+		cardinal = "east"
 	elif direction.x == 1 and direction.z == -1:
 		$Pivot/AnimatedSprite3D.rotation.y = 0
 		$Pivot/AnimatedSprite3D.play("northeast")
+		cardinal = "northeast"
 	elif direction.x == 0 and direction.z == -1:
 		$Pivot/AnimatedSprite3D.rotation.y = 0
 		$Pivot/AnimatedSprite3D.play("north")
+		cardinal = "north"
 	elif direction.x == -1 and direction.z == -1:
 		$Pivot/AnimatedSprite3D.rotation.y = PI
 		$Pivot/AnimatedSprite3D.play("northeast")
+		cardinal = "northwest"
 	elif direction.x == -1 and direction.z == 0:
 		$Pivot/AnimatedSprite3D.rotation.y = PI
 		$Pivot/AnimatedSprite3D.play("east")
+		cardinal = "west"
 	elif direction.x == -1 and direction.z == 1:
 		$Pivot/AnimatedSprite3D.rotation.y = PI
 		$Pivot/AnimatedSprite3D.play("southeast")
-	
+		cardinal = "southwest"
+
+
 	
 	
 	#if direction != Vector3.ZERO:
@@ -79,3 +156,9 @@ func _physics_process(delta):
 	
 
 
+
+
+func _on_timer_timeout():
+	attacking = false
+	$attack/CollisionShape3D.disabled = true
+	$attack.visible = false
